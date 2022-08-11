66c7c558b68a0cb38d9de68242687ebf24d15eaf
ToddF456 <ToddF456@gmail.com>
2022-08-11 14:33:28 -0400

    Updated File Operations
    updated File Operations with base skeleton.

--- src/main/java/com/skillstorm/image/controllers/FileOperationController.java
+++ src/main/java/com/skillstorm/image/controllers/FileOperationController.java
@@ -1,5 +1,14 @@
 package com.skillstorm.image.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skillstorm.image.services.S3BucketStorageService;
+
+@RestController
 public class FileOperationController {
 
+	@Autowired
+	private S3BucketStorageService s3BucketStorageService;
+	
 }
