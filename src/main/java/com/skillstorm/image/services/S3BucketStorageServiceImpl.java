3d0e95ba6f62a25bebaa159eb8565362096b0c5d
Derek Enlow <theunnamed97@gmail.com>
2022-08-12 09:36:05 -0600

    image upload tested

--- src/main/java/com/skillstorm/image/services/S3BucketStorageServiceImpl.java
+++ src/main/java/com/skillstorm/image/services/S3BucketStorageServiceImpl.java
@@ -15,11 +15,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.PutObjectResult;
 
+@Service
 public class S3BucketStorageServiceImpl implements S3BucketStorageService {
 
 	private static final Logger LOG = LoggerFactory.getLogger(S3BucketStorageServiceImpl.class);
